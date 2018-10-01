2d46a03b49b368f0a2c6d53899326733e6e2bf0f
Matthew Yee <Yeezus@Matthews-MBP.home>
2018-09-30 20:31:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+
 var cards = [
 {
 rank: "queen",
@@ -21,10 +22,11 @@ cardImage: "images/king-of-diamonds.png"
 }
 ];
 
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } 	else {
 	alert("Sorry, try again.");
@@ -32,14 +34,26 @@ var checkForMatch = function() {
 
 }
 
-var flipCard = function(cardId) {
-	checkForMatch();
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
+		if (cardsInPlay.length === 2) {
+			checkForMatch();
+}
+	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
 
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-flipCard(0);
-flipCard(2);
+createBoard();
